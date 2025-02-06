09c48647d03eee43d4497ab89ffc9e83388b4ae3
MValenciaolartre <124168851+MValenciaolartre@users.noreply.github.com>
2025-02-06 13:38:13 -0300

    ...
    ..

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -29,7 +29,7 @@ const Login = () => {
       if (data.success) {
         // Store token in localStorage
         localStorage.setItem("token", data.token); // Store token in localStorage
-        localStorage.setItem("user",correo) ;
+        localStorage.setItem("user",data.nombre) ;
         navigate("/admin/dashboard"); // Redirect to the dashboard or home page
       } else {
         setError(data.error || "Error al iniciar sesión. Intente nuevamente.");
