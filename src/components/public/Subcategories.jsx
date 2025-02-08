657be8d2e185077d821def9e2d32656c3ae5fed3
MValenciaolartre <124168851+MValenciaolartre@users.noreply.github.com>
2025-02-08 10:47:19 -0300

    ...
    ...

---
+++ src/components/public/Subcategories.jsx
@@ -0,0 +1,52 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default function SubcategoriesList() {
+  const { idcategoria } = useParams();
+  const [subcategories, setSubcategories] = useState([]);
+
+  const API_SUBCATEGORIES = process.env.REACT_APP_API + `subcategorias.php?endpoint=subcategoria&idcategoria=${idcategoria}`;
+
+  useEffect(() => {
+    fetch(API_SUBCATEGORIES)
+      .then((response) => response.json())
+      .then((data) => {
+        if (Array.isArray(data.subcategoria)) {
+          setSubcategories(data.subcategoria);
+        } else {
+          console.error("Unexpected response structure:", data);
+        }
+      })
+      .catch((error) => console.error("Error fetching subcategories:", error));
+  }, [idcategoria]);
+
+  return (
+    <div className="container py-5">
+      <h2 className="text-center">Subcategorías</h2>
+      <div className="row g-4">
+        {subcategories.length > 0 ? (
+          subcategories.map((subcategory) => (
+            <div key={subcategory.idsubcategoria} className="col-md-6 col-lg-4">
+              <div className="card border-0 rounded-4 shadow-lg">
+                {subcategory.imagen && (
+                  <img
+                    src={process.env.REACT_APP_BASE_URL + subcategory.imagen}
+                    className="card-img-top"
+                    alt={subcategory.nombre}
+                    style={{ objectFit: "cover", height: "150px" }}
+                  />
+                )}
+                <div className="card-body text-center">
+                  <h5 className="card-title">{subcategory.nombre}</h5>
+                </div>
+              </div>
+            </div>
+          ))
+        ) : (
+          <p className="text-center">Cargando subcategorías...</p>
+        )}
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/Subcategories.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Footer, Navbar } from "../components"
+import SubcategoriesList from '../components/public/Subcategories'
+
+const Subcategories = () => {
+  return (
+    <>
+      <Navbar />
+      <SubcategoriesList />
+      <Footer />
+    </>
+  )
+}
+
+export default Subcategories
