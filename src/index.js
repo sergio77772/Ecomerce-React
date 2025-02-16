b36e4f172aca9100d9b364c93dd760131e79aedd
MValenciaolartre <124168851+MValenciaolartre@users.noreply.github.com>
2025-02-16 16:24:31 -0300

    ...
    ...

--- src/index.js
+++ src/index.js
@@ -23,8 +23,6 @@ import {
 import AdminProducts from "./pages/admin/AdminProducts"; // Ajusta la ruta según tu estructura
 import AdminCategory  from "./pages/admin/AdminCategory"; // Ajusta la ruta según tu estructura
 import AdminProveedor  from "./pages/admin/AdminProveedor"; 
-import AdminCliente  from "./pages/admin/AdminCliente"; 
-import AdminVenta  from "./pages/admin/AdminVenta"; 
 import AdminSubCategory  from "./pages/admin/AdminSubCategory";
 import  AdminDashboard from "./pages/admin/AdminDashboard" ;//dashboard de administracion
 import AdminBitacora from "./pages/admin/AdminBitacora";
@@ -85,9 +83,8 @@ root.render(
           <Route path="/admin/bitacora" element={<AdminBitacora />} />
           <Route path="/admin/mesa" element={<AdminMesa />} />
           <Route path="/admin/comercio" element={<AdminComercio />} />
-
-     {/*     <Route path="/admin/Localidades" element={<AdminLocalidades />} />
-          <Route path="/admin/logsDeSistema" element={<AdminLogsDeSistema />} /> */}
+       <Route path="/admin/Localidades" element={<AdminLocalidades />} />
+          <Route path="/admin/logsDeSistema" element={<AdminLogsDeSistema />} />
           <Route path="/admin/ordenes" element={<AdminOdenes />} />
           <Route path="/product/*" element={<PageNotFound />} />
         </Routes>
