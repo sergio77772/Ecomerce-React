3f1b342156f33e2b18fc3ec29a39da9ef43bfcd4
MValenciaolartre <124168851+MValenciaolartre@users.noreply.github.com>
2025-02-24 01:34:10 -0300

    ..
    ..

--- src/components/public/NavigationBar.jsx
+++ src/components/public/NavigationBar.jsx
@@ -8,18 +8,15 @@ export default function BreadcrumbNavigation({ category, subcategory }) {
     <div className="container my-3">
       <nav aria-label="breadcrumb">
         <ol className="breadcrumb">
-          <li className="breadcrumb-item">
-            <a
-             
-              onClick={(e) => {
-                e.preventDefault();
-                navigate('/');
-              }}
-              className="text-dark fw-bold text-uppercase"
-            >
-              {category}
-            </a>
-          </li>
+        <li className="breadcrumb-item">
+  <button
+    onClick={() => navigate('/')}
+    className="text-dark fw-bold text-uppercase bg-transparent border-0 p-0"
+    style={{ textDecoration: "none", cursor: "pointer" }}
+  >
+    {category}
+  </button>
+</li>
           {subcategory && (
             <li className="breadcrumb-item active text-dark" aria-current="page">
               {subcategory}
